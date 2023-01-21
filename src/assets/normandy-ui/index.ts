88560bf3151e48bd9e4492b282e51020b9aa0dd9
The Normandy Project <thenormandyproject@users.noreply.github.com>
2023-01-21 17:47:16 +0000

    New SVGs

---
+++ src/assets/normandy-ui/index.ts
@@ -0,0 +1,29 @@
+import { ReactComponent as N7Logo } from "./assets/N7.svg";
+import { ReactComponent as RenegadeLogo } from "./assets/Renegade.svg";
+import { ReactComponent as ParagonLogo } from "./assets/Paragon.svg";
+import { ReactComponent as AllianceLogo } from "./assets/Alliance.svg";
+import { ReactComponent as CerberusLogo } from "./assets/Cerberus.svg";
+import { ReactComponent as NormandyLogo } from "./assets/Normandy.svg";
+import { ReactComponent as NormandyLetterN } from "./assets/Normandy/N.svg";
+import { ReactComponent as NormandyLetterO } from "./assets/Normandy/O.svg";
+import { ReactComponent as NormandyLetterR } from "./assets/Normandy/R.svg";
+import { ReactComponent as NormandyLetterM } from "./assets/Normandy/M.svg";
+import { ReactComponent as NormandyLetterA } from "./assets/Normandy/A.svg";
+import { ReactComponent as NormandyLetterD } from "./assets/Normandy/D.svg";
+import { ReactComponent as NormandyLetterY } from "./assets/Normandy/Y.svg";
+
+export {
+    N7Logo,
+    RenegadeLogo,
+    ParagonLogo,
+    AllianceLogo,
+    CerberusLogo,
+    NormandyLogo,
+    NormandyLetterN,
+    NormandyLetterO,
+    NormandyLetterR,
+    NormandyLetterM,
+    NormandyLetterA,
+    NormandyLetterD,
+    NormandyLetterY
+}
